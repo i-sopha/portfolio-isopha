0e611287b711f1d522aacd9ab34ddc747516199f
Isabel Sopha <i.sopha94@gmail.com>
2023-04-15 15:22:39 -0700

    changes owl icon to moon

--- src/components/Header.js
+++ src/components/Header.js
@@ -67,7 +67,7 @@ class Header extends Component {
               uncheckedIcon={
                 <span
                   className="iconify"
-                  data-icon="twemoji:owl"
+                  data-icon="emojione:crescent-moon"
                   data-inline="false"
                   style={{
                     display: "block",
